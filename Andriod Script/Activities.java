2755db2b5aa5718def10ddc5bfa8d9e23346b62a
Narasimha1267 <varman@syrahealth.com>
2025-04-21 16:38:18 +0530

    Create Activities.java

---
+++ Andriod Script/Activities.java
@@ -0,0 +1,41 @@
+package SyraHealth;
+
+import java.io.File;
+import java.net.URI;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+public class Activities extends Server
+{
+	@Test
+	
+	 public void ActiveSection() throws Exception, Throwable {
+		
+LoginB2B();
+findElementByIDs("com.android.permissioncontroller:id/permission_allow_button").click();
+findElementByXpath("hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.widget.Button").click();
+
+
+findElementByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[3]/android.view.View[2]").click();
+findElementByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.widget.EditText").sendKeys("Sleep Hygiene"); 
+findElementByAccessibilityId("search").click();
+
+findElementByAccessibilityId("Stimulus Control for Better Sleep").click();
+findElementByAccessibilityId("star_icon").click();
+findElementByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[3]/android.view.View/android.view.View[1]/android.view.View[2]").click();
+ScrollToEndAction();
+Thread.sleep(1000);
+ScrollToEndAction();
+
+
+	}
+	
+}
